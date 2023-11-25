e672435f674348aff158f374df5f4da60dc14a7b
tomaspm98 <up202006845@up.pt>
2023-11-25 19:07:42 +0000

    Menu + funcionalities on the lists

--- list_writer/LWW/lww.py
+++ list_writer/LWW/lww.py
@@ -1,5 +1,7 @@
 import time
 import uuid
+import json
+import os
 
 
 class ShoppingList:
@@ -8,12 +10,21 @@ class ShoppingList:
         self.remove_set = {}
         self.uuid = uuid.uuid4()
 
-    def add(self, element):
-        try:
-            if self.add_set.get(element, 0) < time.time():
-                self.add_set[element] = time.time()
-        except TypeError as error:
-            print(str(error))
+    def add(self, item):
+        item_id = item['id']
+        quantity = item.get('quantity', 1)  
+        acquired = item.get('acquired', 'false')  
+
+        if item_id not in self.add_set or self.add_set[item_id]['timestamp'] < time.time():
+            self.add_set[item_id] = {
+                'timestamp': time.time(),
+                'quantity': quantity,
+                'acquired': acquired
+            }
+        else:
+            # Update quantity and acquired status if item is already in the list
+            self.add_set[item_id]['quantity'] += quantity
+            self.add_set[item_id]['acquired'] = acquired
 
     def lookup(self, element):
         if element not in self.add_set:
@@ -22,18 +33,43 @@ class ShoppingList:
         if element not in self.remove_set:
             return True
 
-        if self.remove_set[element] < self.add_set[element]:
+        # Compare the timestamps
+        if self.remove_set[element]['timestamp'] < self.add_set[element]['timestamp']:
             return True
 
         return False
 
-    def remove(self, element):
+
+    def remove(self, item_id):
+        current_time = time.time()
         try:
-            if self.remove_set.get(element, 0) < time.time():
-                self.remove_set[element] = time.time()
+            if item_id in self.add_set and (item_id not in self.remove_set or self.remove_set[item_id]['timestamp'] < current_time):
+                self.remove_set[item_id] = {
+                    'timestamp': current_time,
+                    'quantity': self.add_set[item_id]['quantity'],
+                    'acquired': self.add_set[item_id]['acquired']
+                }
+            else:
+                raise TypeError("\nItem not in list")    
         except TypeError as error:
             print(str(error))
 
+    def acquire(self, item_id):
+        current_time = time.time()
+        try:
+            if item_id in self.add_set and (item_id not in self.remove_set or self.remove_set[item_id]['timestamp'] < current_time):
+                if (self.add_set[item_id]['quantity'] == '1'):
+                    self.add_set[item_id]['quantity'] = '0'
+                    self.add_set[item_id]['acquired'] = 'true'
+                if (self.add_set[item_id]['quantity'] == '0'):
+                    raise TypeError("\nItem already acquired")    
+                else:    
+                    self.add_set[item_id]['quantity'] = str(int(self.add_set[item_id]['quantity']) - 1)
+            else:
+                raise TypeError("\nItem not in list")    
+        except TypeError as error:
+            print(str(error))
+    
     def compare(self, other):
         add_subset = set(self.add_set.keys()).issubset(other.add_set.keys())
 
@@ -44,25 +80,91 @@ class ShoppingList:
     def merge(self, other):
         merged_list = ShoppingList()
 
-        merged_list.add_set = {**self.add_set, **other.add_set}
+        # Merge add_set
+        for element, details in self.add_set.items():
+            if element in other.add_set:
+                # Compare timestamps and choose the one with the later timestamp
+                if details['timestamp'] > other.add_set[element]['timestamp']:
+                    merged_list.add_set[element] = details
+                else:
+                    merged_list.add_set[element] = other.add_set[element]
+            else:
+                merged_list.add_set[element] = details
 
-        merged_list.remove_set = {**self.remove_set, **other.remove_set}
+        # Also merge add_set from the other list
+        for element, details in other.add_set.items():
+            if element not in merged_list.add_set:
+                merged_list.add_set[element] = details
 
-        for element, timestamp in self.add_set.items():
-            merged_list.add_set[element] = max(merged_list.add_set[element], timestamp)
+        # Merge remove_set in a similar manner
+        for element, details in self.remove_set.items():
+            if element in other.remove_set:
+                if details['timestamp'] > other.remove_set[element]['timestamp']:
+                    merged_list.remove_set[element] = details
+                else:
+                    merged_list.remove_set[element] = other.remove_set[element]
+            else:
+                merged_list.remove_set[element] = details
 
-        for element, timestamp in self.remove_set.items():
-            merged_list.remove_set[element] = max(
-                merged_list.remove_set[element], timestamp
-            )
+        for element, details in other.remove_set.items():
+            if element not in merged_list.remove_set:
+                merged_list.remove_set[element] = details
 
         return merged_list
 
     def get_full_list(self):
-        list_elements = list(self.add_set.keys() | self.remove_set.keys())
         full_list = []
-        for e in list_elements:
-            if self.lookup(e):
-                full_list.append(e)
-
+        for item_id in self.add_set.keys():
+            if self.lookup(item_id):
+                item_details = self.add_set[item_id]
+                full_list.append({
+                    'id': item_id,
+                    'acquired': item_details['acquired'],
+                    'quantity': item_details['quantity']
+                })
         return full_list
+    
+    def print_list(self):
+        print("\nYour list:\n")
+        for item in self.get_full_list():
+            print("ITEM",item)
+    
+    def convert_to_json_format(self):
+        list_items = []
+
+        for item in self.get_full_list():
+            list_item = {
+                "id": item['id'],
+                "acquired": item['acquired'], 
+                "quantity": item['quantity'],       
+            }
+            list_items.append(list_item)
+
+        list_json = {
+            "version": "1.0",
+            "list": list_items
+        }
+        
+        return json.dumps(list_json, indent=4)
+
+
+    def save_list_to_file(self, id, hasName=True):
+        json_data = self.convert_to_json_format()
+        if (hasName == False):
+            filename = f"lists/list_"+str(self.uuid)+".json"
+        else:
+            filename = f"lists/list_"+id+".json"
+
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+        with open(filename, 'w') as file:
+            file.write(json_data)
+        print(f"\nList saved as {filename}")
+        
+    def load_list_from_file(self, filename):
+        filename = f"lists/"+filename
+        with open(filename, 'r') as file:
+            json_data = json.load(file)
+            for item in json_data["list"]:
+                self.add(item)
+            print(f"List loaded from {filename}")    
---
+++ menu.py
@@ -0,0 +1,97 @@
+import sys
+import os
+from list_writer.LWW.lww import ShoppingList
+
+def display_menu():
+    print("Menu:")
+    print("1. Create list")
+    print("2. Print list")
+    print("3. Add item to list")
+    print("4. Remove item from list")
+    print("5. Acquire item")
+    print("0. Exit")
+
+def get_user_choice():
+    choice = input("Enter your choice: ")
+    return choice
+
+def process_choice(choice):
+    if choice == "1":
+        shopping_list = ShoppingList()  
+        shopping_list.save_list_to_file('',False)
+        print("List created:", shopping_list.uuid)
+        print("\n")
+    elif choice == "2":
+        list_id = input("Insert the list id:")
+        for filename in os.listdir('lists'):
+            if filename == "list_"+list_id+".json":
+                shopping_list = ShoppingList()
+                shopping_list.load_list_from_file(filename)
+                shopping_list.print_list()
+                print("\n")
+                    
+    elif choice == "3":
+        list_id = input("Insert the list id:")
+        for filename in os.listdir('lists'):
+            if filename == "list_"+list_id+".json":
+                    item_id = input("Insert the item id:")
+                    item_acquired = 'false'
+                    item_quantity = input("Insert the item quantity:")
+                    item = {}
+                    item['id']=item_id
+                    item['acquired']=item_acquired
+                    item['quantity']=item_quantity
+                    shopping_list = ShoppingList()
+                    shopping_list_old = ShoppingList()
+                    shopping_list.load_list_from_file(filename)
+                    shopping_list_old.load_list_from_file(filename)
+                    shopping_list.add(item)
+                    shopping_list.merge(shopping_list_old)
+                    shopping_list.print_list()
+                    shopping_list.save_list_to_file(list_id, True)
+                    print("\n")
+                    
+    elif choice == "4":
+        list_id = input("Insert the list id:")
+        for filename in os.listdir('lists'):
+            if filename == "list_"+list_id+".json":
+                    item_id = input("Insert the item id:")
+                    shopping_list = ShoppingList()
+                    print("shopping_list",shopping_list.uuid)
+                    shopping_list_old = ShoppingList()
+                    shopping_list.load_list_from_file(filename)
+                    shopping_list_old.load_list_from_file(filename)
+                    shopping_list.remove(item_id)
+                    shopping_list.merge(shopping_list_old)
+                    shopping_list.print_list()   
+                    shopping_list.save_list_to_file(list_id, True)
+                    print("\n")    
+    elif choice == "5":
+        list_id = input("Insert the list id:")
+        for filename in os.listdir('lists'):
+            if filename == "list_"+list_id+".json":
+                    item_id = input("Insert the item id:")
+                    shopping_list = ShoppingList()
+                    shopping_list_old = ShoppingList()
+                    shopping_list.load_list_from_file(filename)
+                    shopping_list_old.load_list_from_file(filename)
+                    shopping_list.acquire(item_id)
+                    shopping_list.merge(shopping_list_old)
+                    shopping_list.print_list()  
+                    shopping_list.save_list_to_file(list_id, True)
+                    print("\n")                       
+    elif choice == "0":
+        print("Exiting...")
+    else:
+        print("Invalid choice")
+
+def main():
+    while True:
+        display_menu()
+        choice = get_user_choice()
+        process_choice(choice)
+        if choice == "0":
+            break
+
+if __name__ == "__main__":
+    main()
